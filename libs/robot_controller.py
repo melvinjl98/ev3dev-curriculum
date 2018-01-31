ab5f57927a3bfc565472f12543fc85d0ebab61e9
mckeenms <mckeenms@rose-hulman.edu>
2018-01-31 11:04:10 -0500

    m4 library function done

--- libs/robot_controller.py
+++ libs/robot_controller.py
@@ -30,4 +30,9 @@ class Snatch3r(object):
         self.left_motor.wait_while(ev3.Motor.STATE_RUNNING)
         self.right_motor.wait_while(ev3.Motor.STATE_RUNNING)
 
-#    def turn_degrees(self, degrees_to_turn, turn_speed_sp):
+    def turn_degrees(self, degrees_to_turn, turn_speed_sp):
+        """"""
+        self.left_motor.run_to_rel_pos(speed_sp=turn_speed_sp, position_sp=degrees_to_turn)
+        self.right_motor.run_to_rel_pos(speed_sp=turn_speed_sp, position_sp=degrees_to_turn)
+        self.left_motor.wait_while(ev3.Motor.STATE_RUNNING)
+        self.right_motor.wait_while(ev3.Motor.STATE_RUNNING)
