fd67c36e849c30eafe76b565f5dbf4983f4d5503
makinewg <makinewg@rose-hulman.edu>
2018-02-05 01:12:16 -0500

    m5 library done

--- libs/robot_controller.py
+++ libs/robot_controller.py
@@ -87,3 +87,6 @@ class Snatch3r(object):
         arm_motor.run_to_abs_pos(position_sp=0, speed_sp=MAX_SPEED)
         arm_motor.wait_while(ev3.Motor.STATE_RUNNING)  # Blocks until the motor finishes running
         ev3.Sound.beep().wait()
+
+    def shutdown(self):
+
