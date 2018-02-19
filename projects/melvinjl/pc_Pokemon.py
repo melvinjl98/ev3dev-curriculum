5cabd7335cda0482f21f6962eee04ba6d9f7d8bb
melvinjl <melvinjl@rose-hulman.edu>
2018-02-19 14:38:42 -0500

    project progress

--- projects/melvinjl/pc_Pokemon.py
+++ projects/melvinjl/pc_Pokemon.py
@@ -1,4 +1,10 @@
-#   Author: Josh Melvin
+"""
+    Author: Josh Melvin
+    CSSE 120 Final Project
+    Pokemon Red: Remastered Edition
+    My project is recreation of the classic Gameboy Color game, Pokemon Red.
+
+"""
 import time
 import random
 
@@ -39,27 +45,11 @@ def main():
 
     party_frame = ttk.Frame(root, padding=0, relief='raised')
     party_frame.grid()
-
+    walk_speed = 300
     start_window(root, windows_and_number)
 
-    photo = tkinter.PhotoImage(file="red_start.gif")
-
-    press_start = ttk.Button(main_frame, image=photo)
-    press_start.image = photo
-    press_start.grid()
-    press_start['command'] = lambda: start.destroy()
-    start.mainloop()
-
-    walk_speed = 300
-
-    movement = tkinter.Tk()
-    movement.bind('<Up>', lambda event: forward_callback(mqtt_client, walk_speed, walk_speed))
-    movement.bind('<Left>', lambda event: left_callback(mqtt_client, walk_speed, walk_speed))
-    movement.bind('<space>', lambda event: stop_callback(mqtt_client))
-    movement.bind('<Right>', lambda event: right_callback(mqtt_client, walk_speed, walk_speed))
-    movement.bind('<Down>', lambda event: back_callback(mqtt_client, walk_speed, walk_speed))
-    movement.bind('<u>', lambda event: send_up(mqtt_client))
-    movement.bind('<j>', lambda event: send_down(mqtt_client))
+    movement(mqtt_client, windows_and_number, walk_speed)
+    party_window(mqtt_client, windows_and_number)
 
     Charmander = Pokemon()
     Charmander.type = "Fire"
@@ -68,30 +58,6 @@ def main():
     Bulbasaur = Pokemon()
     Bulbasaur.type = "Grass"
     party = [Charmander, Squirtle, Bulbasaur]
-
-    charmander = tkinter.PhotoImage(file="Charmander.gif")
-    squirtle = tkinter.PhotoImage(file="Squirtle.gif")
-    bulbasaur = tkinter.PhotoImage(file="Bulbasaur.gif")
-
-    Charmander = ttk.Button(party_frame, image=charmander)
-    Charmander.image = charmander
-    Charmander.grid(row=0, column=0)
-
-    Squirtle = ttk.Button(party_frame, image=squirtle)
-    Squirtle.image = squirtle
-    Squirtle.grid(row=0, column=1)
-
-    Bulbasaur = ttk.Button(party_frame, image=bulbasaur)
-    Bulbasaur.image = bulbasaur
-    Bulbasaur.grid(row=1, column=0)
-
-    q_button = ttk.Button(main_frame, text="Quit")
-    q_button.grid(row=4, column=2)
-    q_button['command'] = (lambda: quit_program(mqtt_client, False))
-
-    e_button = ttk.Button(main_frame, text="Exit")
-    e_button.grid(row=5, column=2)
-    e_button['command'] = (lambda: quit_program(mqtt_client, True))
 
 
 def forward_callback(mqtt_client, left_speed, right_speed):
@@ -132,6 +98,7 @@ def quit_program(mqtt_client, shutdown_ev3):
     mqtt_client.close()
     exit()
 
+
 def start_window(root, windows):
     """ Puts Buttons on the main window. """
     root.title("                            Pokemon Red: Remastered Edition")
@@ -143,6 +110,8 @@ def start_window(root, windows):
     press_start.image = tkinter.PhotoImage(file="red_start.gif")
     press_start.grid()
     press_start['command'] = lambda: change_number(windows, 1)
+
+
 """
     destroy_button = ttk.Button(window1_frame,
                                 text='Destroy some windows')
@@ -150,36 +119,50 @@ def start_window(root, windows):
     destroy_button['command'] = lambda: destroy_windows(windows)
 """
 
-def put_stuff_on_window2(windows_and_number):
-    """ Puts Buttons on a secondary window. """
-    root2 = tkinter.Toplevel()  # Note Toplevel, NOT Tk.
 
-    window2_frame = ttk.Frame(root2, padding=20)
-    window2_frame.grid()
+def movement(mqtt_client, windows_and_number, walk_speed):
+    """Creates an invisible window that binds robot movement to keyboard controls."""
+    movement_win = tkinter.Toplevel()
 
-    decrease_button = ttk.Button(window2_frame,
-                                 text='Decrease the number')
-    decrease_button.grid()
-    decrease_button['command'] = lambda: change_number(windows_and_number, -1)
-
-    button_text = 'Pop up a new window with the number'
-    pop_up_button = ttk.Button(window2_frame, text=button_text)
-    pop_up_button.grid()
-    pop_up_button['command'] = lambda: pop_up(windows_and_number)
+    movement_win.bind('<Up>', lambda event: forward_callback(mqtt_client, walk_speed, walk_speed))
+    movement_win.bind('<Down>', lambda event: back_callback(mqtt_client, walk_speed, walk_speed))
+    movement_win.bind('<Left>', lambda event: left_callback(mqtt_client, walk_speed, walk_speed))
+    movement_win.bind('<Right>', lambda event: right_callback(mqtt_client, walk_speed, walk_speed))
+    movement_win.bind('<space>', lambda event: stop_callback(mqtt_client))
+    movement_win.bind('<u>', lambda event: send_up(mqtt_client))
+    movement_win.bind('<j>', lambda event: send_down(mqtt_client))
 
 
-def put_stuff_on_window3(windows_and_number):
-    """ Puts a Label on a secondary window. """
-    root3 = tkinter.Toplevel()  # Note Toplevel, NOT Tk.
+def party_window(mqtt_client, windows_and_number):
+    """Create a window that displays your Pokemon party."""
+    party = tkinter.Toplevel()
 
-    window3_frame = ttk.Frame(root3, padding=20)
-    window3_frame.grid()
+    party_frame = ttk.Frame(party, padding=20)
+    party_frame.grid()
 
-    number_label = ttk.Label(window3_frame, text='The number is: 0')
-    number_label.grid()
+    charmander = tkinter.PhotoImage(file="Charmander.gif")
+    squirtle = tkinter.PhotoImage(file="Squirtle.gif")
+    bulbasaur = tkinter.PhotoImage(file="Bulbasaur.gif")
 
-    windows_and_number.number = 0
-    windows_and_number.label = number_label
+    Charmander = ttk.Button(party_frame, image=charmander)
+    Charmander.image = charmander
+    Charmander.grid(row=0, column=0)
+
+    Squirtle = ttk.Button(party_frame, image=squirtle)
+    Squirtle.image = squirtle
+    Squirtle.grid(row=0, column=1)
+
+    Bulbasaur = ttk.Button(party_frame, image=bulbasaur)
+    Bulbasaur.image = bulbasaur
+    Bulbasaur.grid(row=1, column=0)
+
+    q_button = ttk.Button(party_frame, text="Quit")
+    q_button.grid(row=4, column=2)
+    q_button['command'] = (lambda: quit_program(mqtt_client, False))
+
+    e_button = ttk.Button(party_frame, text="Exit")
+    e_button.grid(row=5, column=2)
+    e_button['command'] = (lambda: quit_program(mqtt_client, True))
 
 
 def change_number(windows_and_number, delta):
@@ -209,4 +192,3 @@ def destroy_windows(data):
 
 
 main()
-
